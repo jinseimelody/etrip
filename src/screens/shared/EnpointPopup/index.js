3b93ab87633e699ee1258e4094f96f1db62b2c77
dat.nguyen <jinseimelody@gmail.com>
2022-08-01 18:01:06 +0700

    update endpoint_select_popup component

--- src/screens/shared/EnpointPopup/index.js
+++ src/screens/shared/EnpointPopup/index.js
@@ -1,56 +1,75 @@
-import React, {forwardRef, memo, useEffect, useImperativeHandle, useReducer, useRef} from 'react';
+import React, {
+  forwardRef,
+  memo,
+  useEffect,
+  useImperativeHandle,
+  useReducer,
+  useRef,
+  useState
+} from 'react';
 import classNames from 'classnames/bind';
 
 import images from '~/assets';
 import style from './endpoint.popup.module.scss';
-import {keyboard} from '~/helper';
 import {endpointApi} from '~/api';
 import {Modal} from '~/components';
 
 const cx = classNames.bind(style);
 const initState = {
-  searchPattern: '',
+  search: '',
   value: null,
   endpoints: []
 };
 
-const actions = {
-  setEndpoints: 'setEndpoints',
-  setSearchPattern: 'setSearchPattern',
-  all: 'all'
-};
+const CLEAR = 'clear';
+const SET_SEARCH = 'setsearch';
+const SET_VALUE = 'setvalue';
+const SET_ENDPOINTS = 'setendpoint';
+
+const clear = _ => ({type: CLEAR});
+const setSearch = payload => ({type: SET_SEARCH, payload});
+const setValue = payload => ({type: SET_VALUE, payload});
+const setEndpoints = payload => ({type: SET_ENDPOINTS, payload});
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case actions.setEndpoints:
+    case CLEAR:
+      return {search: '', value: null, endpoints: []};
+    case SET_SEARCH:
+      return {...state, search: action.payload};
+    case SET_VALUE:
+      return {...state, value: action.payload};
+    case SET_ENDPOINTS:
       return {...state, endpoints: action.payload};
-    case actions.setSearchPattern:
-      return {...state, searchPattern: action.payload};
-    case actions.all:
-      return action.payload;
     default:
       throw new Error(`action ${action} is invalid`);
   }
 };
 
 const EnpointPopup = forwardRef((props, ref) => {
-  const {endpoint} = props;
-  const typingTimerRef = useRef();
+  const {onSelect, endpoint} = props;
   const modalRef = useRef();
+  const typingTimerRef = useRef();
 
   const [state, dispatch] = useReducer(reducer, initState, () => ({
     ...initState,
-    searchPattern: endpoint && endpoint.name,
+    search: (endpoint && endpoint.name) || '',
     value: endpoint
   }));
 
+  // monitor search text then call api
   useEffect(() => {
-    if (!state.searchPattern) {
-      endpointApi.search({q: ''}).then(endpoints => {
-        dispatch({type: actions.setEndpoints, payload: endpoints});
-      });
-    }
-  }, [state.searchPattern]);
+    typingTimerRef && clearTimeout(typingTimerRef.current);
+    typingTimerRef.current = setTimeout(async () => {
+      const endpoints = await endpointApi.search({q: state.search});
+      console.log('get');
+      dispatch(setEndpoints(endpoints));
+    }, 500);
+  }, [state.search]);
+
+  useEffect(() => {
+    onSelect && state.value && state.onSelect(state.value);
+  }, [state.value]);
 
   useImperativeHandle(ref, () => ({
     show: () => {
@@ -61,58 +80,25 @@ const EnpointPopup = forwardRef((props, ref) => {
     }
   }));
 
-  const handleTyping = e => {
-    typingTimerRef && clearTimeout(typingTimerRef.current);
-    if (!keyboard.isNormalKeys(e)) {
-      dispatch({type: actions.setSearchPattern, payload: e.target.value});
-      return;
-    }
-
-    typingTimerRef.current = setTimeout(async () => {
-      const endpoints = await endpointApi.search({q: e.target.value});
-      dispatch({type: actions.setEndpoints, payload: endpoints});
-    }, 500);
-  };
-
-  const handleClear = () => {
-    dispatch({
-      type: actions.all,
-      payload: {
-        searchPattern: null,
-        value: null,
-        endpoints: []
-      }
-    });
-  };
-
-  const handleSelect = ep => {
-    // setValue(ep);
-  };
-
   return (
     <Modal ref={modalRef} cancel="Hủy" title="Ngày khởi hành">
       <div className={cx('endpoint-search-box')}>
         <input
           type="text"
           placeholder="Origin location"
-          value={state.searchPattern}
-          onChange={handleTyping}
+          value={state.search}
+          onChange={e => dispatch(setSearch(e.target.value))}
         />
-        {state.searchPattern && state.searchPattern.length > 0 && (
-          <span onClick={handleClear} className={cx('btn-cancel')}></span>
+        {state.search.length > 0 && (
+          <span onClick={() => dispatch(clear())} className={cx('btn-cancel')}></span>
         )}
       </div>
 
       {state.endpoints && state.endpoints.length > 0 && (
-        <div className={cx('endpoint-resuls')}>
+        <div className={cx('endpoint-result')}>
           <div className={cx('group-name')}>Địa điểm phổ biến</div>
           {state.endpoints.map((ep, i) => (
-            <div
-              key={i}
-              onClick={() => {
-                handleSelect(ep);
-              }}
-              className={cx('group-item')}>
+            <div key={i} onClick={() => dispatch(setValue(ep))} className={cx('group-item')}>
               <div className={cx('decorator')}>
                 <img src={images.marker} alt="" />
               </div>
